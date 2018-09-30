5c35d20a9b053e2441f9cb30981d9b3a32f084ea
Sephirothxlx <1216083287@qq.com>
2018-09-30 00:34:01 -0500

    commit

---
+++ algorithm/combination/LC90Subsets2.java
@@ -0,0 +1,32 @@
+package algorithm.combination;
+
+public class LC90Subsets2 {
+	public void combine(int[] nums, int start, List<List<Integer>> l, List<Integer> al, int k) {
+		if (al.size() == k) {
+			List<Integer> temp = new ArrayList<Integer>(al);
+			l.add(temp);
+		} else {
+			int d = nums[0] - 1;
+			for (int i = start; i < nums.length; i++) {
+				if (d == nums[i])
+					continue;
+				else {
+					d = nums[i];
+					al.add(nums[i]);
+					combine(nums, i + 1, l, al, k);
+					al.remove(al.size() - 1);
+				}
+			}
+		}
+	}
+
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> l = new ArrayList<List<Integer>>();
+		List<Integer> al = new ArrayList<Integer>();
+		for (int i = 0; i <= nums.length; i++) {
+			combine(nums, 0, l, al, i);
+		}
+		return l;
+	}
+}
