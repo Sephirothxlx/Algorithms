4d68415509d9dca13f6703283c641bdfd2a7deb8
Sephirothxlx <1216083287@qq.com>
2018-10-03 00:08:37 -0500

    commit

--- algorithm/tree/LC102BinaryTreeLevelOrderTraversal.java
+++ algorithm/tree/LC102BinaryTreeLevelOrderTraversal.java
@@ -4,12 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LC102BinaryTreeLevelOrderTraversal {
-	public void add(TreeNode root) {
-
+	public void add(TreeNode root, List<List<Integer>> l, int x) {
+		if (root == null)
+			return;
+		List<Integer> ll = null;
+		if (x >= l.size()) {
+			ll = new ArrayList<Integer>();
+			l.add(ll);
+		} else
+			ll = l.get(x);
+		ll.add(root.val);
+		add(root.left, l, x + 1);
+		add(root.right, l, x + 1);
 	}
 
 	public List<List<Integer>> levelOrder(TreeNode root) {
 		List<List<Integer>> l = new ArrayList<List<Integer>>();
+		add(root, l, 0);
 		return l;
 	}
 }
