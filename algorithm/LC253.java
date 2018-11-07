1680f2eabe95e12624cbf3149bae40ef1e5e11ef
Sephirothxlx <1216083287@qq.com>
2018-11-06 22:04:48 -0600

    commit

---
+++ algorithm/LC253.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+public class LC253 {
+	class Interval {
+		int start;
+		int end;
+
+		Interval() {
+			start = 0;
+			end = 0;
+		}
+
+		Interval(int s, int e) {
+			start = s;
+			end = e;
+		}
+	}
+}
