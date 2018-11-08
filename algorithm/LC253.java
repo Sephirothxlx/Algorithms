7c05779680d7f2915177cf1bfb389f38a529aab0
Sephirothxlx <1216083287@qq.com>
2018-11-08 13:41:38 -0600

    commit

--- algorithm/LC253.java
+++ algorithm/LC253.java
@@ -1,5 +1,9 @@
 package algorithm;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class LC253 {
 	class Interval {
 		int start;
@@ -14,5 +18,36 @@ public class LC253 {
 			start = s;
 			end = e;
 		}
+	}
+
+	public int minMeetingRooms(Interval[] intervals) {
+		if(intervals==null||intervals.length==0)
+			return 0;
+		Arrays.sort(intervals,new Comparator<Interval>(){
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				// TODO Auto-generated method stub
+				return o1.start-o2.start;
+			}
+		});
+		PriorityQueue<Interval> q=new PriorityQueue<Interval>(new Comparator<Interval>(){
+
+			@Override
+			public int compare(Interval o1, Interval o2) {
+				// TODO Auto-generated method stub
+				return o1.end-o2.end;
+			}
+		});
+		q.add(intervals[0]);
+		for(int i=1;i<intervals.length;i++){
+			Interval temp=q.poll();
+			if(temp.end<intervals[i].start){
+				temp.end=intervals[i].end;
+			}else{
+				q.add(intervals[i]);
+			}
+			q.add(temp);
+		}
+		return q.size();
 	}
 }
