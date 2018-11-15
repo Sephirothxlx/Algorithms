f3c56148d5bee5aa301c4de3eb918648c5062f04
Sephirothxlx <1216083287@qq.com>
2018-11-15 00:05:48 -0600

    commit

--- algorithm/test/TestConstructor.java
+++ algorithm/test/TestConstructor.java
@@ -1,5 +1,9 @@
 package algorithm.test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class TestConstructor {
 	public static class t {
 		t(int t) {
@@ -15,5 +19,11 @@ public class TestConstructor {
 		TestConstructor tt = new TestConstructor();
 		t g = new TestConstructor.t(1);
 		h gg = tt.new h(1);
+		HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
+		h.put(1, 1);
+		HashMap<Integer,Integer> h1=new HashMap<Integer,Integer>(h);
+	    for(int x:h1.keySet()){
+	    	System.out.println(x);
+	    }
 	}
 }
